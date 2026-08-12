46ad50bd2c8614644203c7d5d072ded1a8148f84
konpong <konpong2006@gmail.com>
2026-08-12 21:46:01 +0700

    Remove non-partner Rayong return bus

--- test/bot.test.js
+++ test/bot.test.js
@@ -66,7 +66,6 @@ test('loads real Korat outbound schedules with bus and driver phones', async () 
 test('loads real Rayong return schedules and keeps unknown driver phones blank', async () => {
   assert.deepEqual((await schedulesFor('RY-KOR', '2026-08-13')).map((item) => `${item.departureTime} ${item.busNumber} ${item.driverPhone}`), [
     '04:00 267-44 ',
-    '05:00 267-13 ',
     '06:00 267-19 091-342-7497',
     '09:00 267-29 096-339-0599',
     '11:00 267-8 ',
